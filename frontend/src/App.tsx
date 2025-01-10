88be929a1d8b81c95a3199bfa4d5215e978fdd5b
Pedro Eduardo Garcia <pedroeduardogarcia@outlook.com>
2025-01-10 17:44:31 -0300

    feat: initial frontend setup
    Signed-off-by: Pedro Eduardo Garcia <pedroeduardogarcia@outlook.com>

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+
+function App() {
+
+  return (
+    <>
+      <h1>Hello</h1>
+    </>
+  )
+}
+
+export default App;
