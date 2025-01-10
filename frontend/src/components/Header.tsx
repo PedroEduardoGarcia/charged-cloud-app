90e8f2b848702845fd0fa54699c875cd872e868a
Pedro Eduardo Garcia <pedroeduardogarcia@outlook.com>
2025-01-10 18:18:08 -0300

    feat: placeholder search bar
    Signed-off-by: Pedro Eduardo Garcia <pedroeduardogarcia@outlook.com>

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,10 +1,12 @@
 import './header.css';
 import Logo from './Logo';
+import SearchBar from './SearchBar';
 
 function Header() {
   return (
     <header id="header" className="header fixed-top d-flex align-items-center">
       <Logo />
+      <SearchBar />
     </header>
   )
 }
